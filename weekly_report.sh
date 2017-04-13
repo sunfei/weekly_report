9bc121acf15f2d13c02aa58cbf54df0d80829156
wangsheng <ws2@meitu.com>
2017-04-14 00:53:55 +0800

    Update output format

--- weekly_report.sh
+++ weekly_report.sh
@@ -1,6 +1,23 @@
-#!/bin/bash
-
-## TODO print all branch commits about the author as "commit list"
-## TODO print the commit information about the author as "项目任务"
 ## TODO publish new report at thursday night
-git log --since '7 days ago' --oneline --author $1 | awk 'BEGIN{FS=OFS=" "}{$1="*";print}'
+
+rm log.txt
+
+echo "\n# 北京影像技术研发周报" > log.txt
+echo "\n## android研发" >> log.txt
+
+git log --since '7 days ago' --oneline --author $1 | awk 'BEGIN{FS=OFS=" "}{$1="*";print}' | grep -v Merge | uniq >> log.txt
+
+## sort log.txt | uniq > log_temp.txt
+
+git log --since '7 days ago' --author $1 | grep "^\ " | grep -v Signed-off-by: | grep -v Merge | grep -v "\[" | uniq > ./log_temp.txt
+
+echo "\n\n## 项目任务" >> log.txt
+
+sort log_temp.txt | sed "s/^[ \t]*//g" | uniq | grep -v http:// >> log.txt
+
+rm log_temp.txt
+
+cat log.txt | while read line
+do
+    echo $line
+done
